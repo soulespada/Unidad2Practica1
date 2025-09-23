c33428cb8f0d781f15e80199179ba5c6ae5962de
Nairdax <nairdax02@hotmail.com>
2025-09-23 22:35:33 +0200

    Hacemos la clase Circulo y la instanciamos en el Main

---
+++ src/main/java/com/ieschabas/Circulo.java
@@ -0,0 +1,36 @@
+package com.ieschabas;
+
+public class Circulo {//9. Crea una clase Circulo con atributo radio y un método calcularArea().
+    private double radio;
+
+    /**
+     * Constructor vacio
+     */
+    public Circulo(){};
+
+    /**
+     * Constructor sobrecargado
+     * @param radio
+     */
+    public Circulo(double radio){radio = this.radio;}
+
+    /**
+     * Metodo que establece el radio del Circulo
+     * @param radio
+     */
+    public void setRadio(double radio){radio=this.radio;}
+
+    /**
+     * Metodo que devuelve el radio del Circulo
+     * @return
+     */
+    public double getRadio(){return this.radio;}
+
+    /**
+     * Metodo (que recibe un objeto de tipo PotenciayRaiz para utilizar su metodo de calcular potencias)que devuelve el area del circulo
+     * @param potenciayRaiz
+     * @return
+     */
+    public double calcularArea(PotenciayRaiz potenciayRaiz){return potenciayRaiz.calcularPotencia(2)*Math.PI;}
+
+}
